b2c401e9b230079052ed4364e8f240483c803bff
MyDreGo <james@mydrego.com>
2017-11-16 20:06:51 +0800

    003

--- app/src/main/java/com/example/student/a20171116_01/MainActivity.java
+++ app/src/main/java/com/example/student/a20171116_01/MainActivity.java
@@ -8,11 +8,12 @@ import android.widget.SimpleAdapter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     ListView lv1;
-    SimpleAdapter  adapter;
-    ArrayList<Map<String,String>> mylist = new ArrayList<>();
+    SimpleAdapter adapter;
+    ArrayList<Map<String, Object>> mylist = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,23 +21,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         lv1 = (ListView) findViewById(R.id.listview);
 
-        HashMap <String,String> m1 = new HashMap<>();
+        HashMap<String, Object> m1 = new HashMap<>();
         m1.put("city", "台北");
         m1.put("code", "02");
+        m1.put("img", R.drawable.n1);
         mylist.add(m1);
 
-        HashMap <String,String> m2 = new HashMap<>();
+        HashMap<String, Object> m2 = new HashMap<>();
         m2.put("city", "台中");
         m2.put("code", "04");
+        m2.put("img", R.drawable.n2);
         mylist.add(m2);
 
-        HashMap<String, String> m3 = new HashMap<>();
+        HashMap<String, Object> m3 = new HashMap<>();
         m3.put("city", "台南");
         m3.put("code", "06");
+        m3.put("img", R.drawable.n3);
         mylist.add(m3);
 
-        adapter = new SimpleAdapter(MainActivity.this,mylist,R.layout.mylayout,new String[] {"city", "code"},
-                new int[] {R.id.textView, R.id.textView2});
+        adapter = new SimpleAdapter(MainActivity.this, mylist, R.layout.mylayout, new String[]{"city", "code" ,"img"},
+                new int[]{R.id.textView, R.id.textview2,R.id.imageView});
         lv1.setAdapter(adapter);
     }
 }
